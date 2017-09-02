49315bf5b19c597ad8f680f33871d367153ccc6a
Nero Siva <neroshan12@gmail.com>
2017-09-02 19:38:48 +0100

    Refactor unit test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,8 @@
 require 'docking_station'
 
-describe DockingStation do
-
+describe DockingStation do      # Describe
+  it { is_expected.to respond_to :release_bike }
+#  describe 'responds to_release bike' do
+#    expect(subject).to respond_to :release_bike
+#  end
 end
