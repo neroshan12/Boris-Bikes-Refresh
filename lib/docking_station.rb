7a4e8d41c02d248ca6522c4dc5970ac58b952247
Nero Siva <neroshan12@gmail.com>
2017-09-03 10:12:45 +0100

    Add instance variables

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,19 @@
 require_relative 'bike'
 
 class DockingStation
+
+  attr_reader :bike
+
   def release_bike
     Bike.new
   end
+
+  def dock(bike)
+#    bike # we need to return the bike we dock
+    @bike = bike
+  end
+
+#  def bike
+#    @bike   # returns the bike we docked
+#  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -9,4 +9,16 @@ describe DockingStation do      # Describe
     bike = subject.release_bike
     expect(bike).to be_working
   end
+
+  it 'docks something' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+#  it { is_expected.to respond_to(:bike)}
+  it 'returs docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike # need to return the bike we just docked
+  end
 end
